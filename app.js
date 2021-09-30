7e973af294f04c52e5c00e920088bfcf3e05b3ab
Mark Voltaire <markjvoltaire@gmail.com>
2021-09-29 20:59:49 -0400

    completed lab

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 // import functions and grab DOM elements
 
+const tazImg = document.getElementById('taz');
+const tazNoise = document.getElementById('tazSound');
+
+const bugsImg = document.getElementById('bugs');
+const bugsAudio = document.getElementById('bugsSound');
+
+const daffyImg = document.getElementById('daffy');
+const daffyAudio = document.getElementById('daffySound');
 // initialize global state
 
 // set event listeners 
+
+tazImg.addEventListener('click', ()=>{
+    tazNoise.play();
+});
+
+bugsImg.addEventListener('click', ()=>{
+    bugsAudio.play();
+});
+
+daffyImg.addEventListener('click', ()=>{
+    daffyAudio.play();
+});
+
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
